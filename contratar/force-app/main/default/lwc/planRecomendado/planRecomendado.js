31c8e015751f434498a1363bb446efd0ffcdd36b
Mariel Marquez <75107742+MarielMarquez@users.noreply.github.com>
2023-06-01 17:55:05 -0300

    Add files via upload

---
+++ contratar/force-app/main/default/lwc/planRecomendado/planRecomendado.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+export default class PlanRecomendado extends LightningElement {
+
+    modal = document.getElementsByClassName("containerVentana");
+    btn = document.getElementsByClassName("contratar");
+
+
+
+}
